71f445157e4bae38cc9a8afe2fc84e444596f65d
Daniel Williams <danielpaulwill@gmail.com>
2022-03-11 21:34:28 -0500

    position on page sorted. 2 placeholder images inserted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='mainContainer'>
+      <img className='appsContainer' src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*"></img>
+      <img className='appsContainer' src="https://cdn.cnn.com/cnnnext/dam/assets/201030094143-stock-rhodesian-ridgeback-exlarge-169.jpg"></img>
+      <div className='appsContainer'></div>
+      <div className='appsContainer'></div>
     </div>
   );
 }
